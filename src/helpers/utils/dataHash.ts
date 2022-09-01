0d0540e4ad0d595b307cccc2cb609e1c20a1d7b2
Rodolfo Iribarren <rodolfo.iribarren@tunubi.com>
2022-09-01 19:11:33 -0300

    added dataHash class & helpers alias

---
+++ src/helpers/utils/dataHash.ts
@@ -0,0 +1,21 @@
+import * as argon2 from "argon2";
+
+class DataHash {
+  static async hash(data: string) {
+    try {
+      const hash = await argon2.hash(data, {timeCost: 3});
+      return hash;
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  static async verify(hash: string, data: string){
+    try {
+      return (await argon2.verify(hash, data))
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
+
+export default DataHash
