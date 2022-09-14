ebec0aa9d53e5f9ff8b31e0624c6b0b600570c6f
Lisbaldy de Jesus <lisbaldy.ojeda@tunubi.com>
2022-09-14 17:36:48 -0300

    🔧 tsconfig file update to activate noImplicitAny.

--- src/middlewares/errors/index.ts
+++ src/middlewares/errors/index.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
-function errorHandler(err, _req: Request, res: Response, _next: NextFunction) {
+function errorHandler(err: any, _req: Request, res: Response, _next: NextFunction) {
   console.log('middleware', err);
   res.status(401).send({
     message: err.message,
