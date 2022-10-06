2a862da023cd361ed957b2a7cd572da4854adc94
Francisco Zapiola <francisco.zapiola@tunubi.com>
2022-10-06 18:06:54 -0300

    🛂 Updated login to check if user had a previous session, and return both access and refresh tokens

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -6,6 +6,9 @@ import jwt from 'jsonwebtoken';
 
 const Users = new UserService();
 
+const ACCESS_TOKEN_EXPIRATION = '1m';
+const REFRESH_TOKEN_EXPIRATION = '2m';
+
 class Auth {
   pool: Pool;
   constructor() {
@@ -23,37 +26,63 @@ class Auth {
     }
     const { password, ...response } = user;
 
-    const accessToken = this.signAccessToken({ user_id: user.id }, '2d');
+    const accessToken = this.signJWT({ user_id: user.id }, ACCESS_TOKEN_EXPIRATION);
+    const refreshToken = this.signJWT({ user_id: user.id }, REFRESH_TOKEN_EXPIRATION);
 
-    await this.saveAccessTokenToDB(accessToken, user.id);
+    if (await this.checkJWTByUserId(user.id)) {
+      await this.updateJWTByUserId(accessToken, refreshToken, user.id);
+    } else {
+      await this.saveJWTToDB(accessToken, refreshToken, user.id);
+    }
 
     return {
       ...response,
       accessToken,
+      refreshToken,
     };
   }
 
-  public signAccessToken(payload: object, expiresIn: string | number) {
+  public signJWT(payload: object, expiresIn: string | number) {
     var token = jwt.sign(payload, process.env.JWT_SECRET ?? '', {
       expiresIn,
     });
     return token;
   }
 
-  //TODO: Add logic for not accumulating expired tokens. Maybe delete old ones, or overwrite when creating new one?
-  public async saveAccessTokenToDB(token: string, user_id: number) {
-    const query = `INSERT INTO "accessToken" (
-      token,
+  public async saveJWTToDB(access_token: string, refresh_token: string, user_id: number) {
+    const query = `INSERT INTO "authToken" (
+      access_token,
+      refresh_token,
       user_id
-    ) VALUES ($1, $2)`;
+    ) VALUES ($1, $2, $3)`;
 
-    const values = [token, user_id];
+    const values = [access_token, refresh_token, user_id];
 
     await this.pool.query(query, values);
   }
 
+  public async updateJWTByUserId(access_token: string, refresh_token: string, user_id: number) {
+    const query = `UPDATE "authToken" SET access_token = $2, refresh_token = $3 WHERE id = $1`;
+
+    const values = [user_id, access_token, refresh_token];
+
+    console.log('UP`DEATEANDOO!!ASDÑLFKASDJÑFL');
+
+    await this.pool.query(query, values);
+  }
+
+  public async checkJWTByUserId(user_id: number) {
+    const query = `SELECT * FROM "authToken" WHERE id = $1`;
+
+    const values = [user_id];
+
+    const result = await this.pool.query(query, values);
+    console.log('result.rows ', result.rows);
+    return Boolean(result.rows.length);
+  }
+
   public async verifyJwt(token: string) {
-    const query = `SELECT * from "accessToken" where `;
+    const query = `SELECT * from "authToken" where `;
   }
 }
 
