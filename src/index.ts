d5163c58d14399b470390f3d998f9ef50bd9f303
Alan Diaz <Alanfdiaz@gmail.com>
2022-11-01 11:05:47 -0300

    ✨ Socket.io Connection example

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,30 @@ import express from 'express';
 import routerApi from '@routes';
 import { errorLogger, boomErrorHandler, genericErrorHandler } from '@middlewares';
 import { activatePassportStrategies } from '@helpers';
-
+import {createServer} from "http"
+import { Server,Socket } from "socket.io";
 const app = express();
+const server = createServer(app)
+export const ioServ = new Server(server,{
+  cors:{origin:"*"}
+})
+ioServ.on("connection", (socket: Socket) => {
+ console.log("socket ID",socket.id)
+ socket.on("incomingMessage",(data)=>{
+  console.log("message",data)
+  ioServ.emit("beResponse",`Recibi ${data}`)})
+  //socket.broadcast.emit("beResponse",`Recibi ${data}`)})
+});
 const port = 8080;
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 activatePassportStrategies();
-
 routerApi(app);
 
 app.use(errorLogger);
 app.use(boomErrorHandler);
 app.use(genericErrorHandler);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`My port: ${port}`);
 });
